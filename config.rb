79b5d40889aa590ecb6c859e2f64d672ac6f4edb
Vasco Ribeiro <vduarterib@icloud.com>
2016-05-02 22:50:55 +0200

    flat dinamic view

--- config.rb
+++ config.rb
@@ -49,6 +49,9 @@ activate :autoprefixer
 #     "Helping"
 #   end
 # end
+["anne", "seb", "romain"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", :locals => { :owner_name => name }, :ignore => true
+end
 
 set :css_dir, 'stylesheets'
 
